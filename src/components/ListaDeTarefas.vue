45db776c11bde5ef1a2c1200314324d00f1204f2
ThiagoComba <thiagocomba@hotmail.com>
2024-03-26 12:59:10 -0300

    componentizando a aplicação

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,22 @@
+<script setup>
+    const props = defineProps(['tarefas']);
+</script>
+
+<template>
+    <ul v-if="props.tarefas.length > 0" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+    <h5 class="mt-3 ms-2" v-else>Nenhuma tarefa pendente</h5>
+</template>
+
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
